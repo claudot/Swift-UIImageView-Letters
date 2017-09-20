25d9ae57f78d698e688f16492be536f3fe2784fa
Oleg Gnidets <OGnidets@Mains-MacBook-Pro-8.local>
2017-09-20 15:03:55 +0300

    Refactoring and migration to Swift 4.

--- Sample/UIImageViewLettersDemoTests/UIImageViewLettersDemoTests.swift
+++ Sample/UIImageViewLettersDemoTests/UIImageViewLettersDemoTests.swift
@@ -10,39 +10,39 @@ import XCTest
 @testable import UIImageViewLettersDemo
 
 class UIImageViewLettersDemoTests: XCTestCase {
-    
+
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-    
+
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
-    
+
     func testShortString() {
-        let full:String = "Paul CLAUDOT"
+        let full: String = "Paul CLAUDOT"
         XCTAssert(full.shortString() == "PC", "Short should return PC")
 
-        let half:String = "Paul"
+        let half: String = "Paul"
         XCTAssert(half.shortString() == "P", "Short should return P")
 
-        let empty:String = ""
+        let empty: String = ""
         XCTAssert(empty.shortString() == "", "Short should return empty string")
 
-        let complex:String = "Paul Anatole CLAUDOT"
+        let complex: String = "Paul Anatole CLAUDOT"
         XCTAssert(complex.shortString() == "PC", "Short should return PC")
-        
-        let min:String = "paul claudot"
+
+        let min: String = "paul claudot"
         XCTAssert(min.shortString() == "PC", "Short should return PC")
     }
-    
+
     func testColorHashString() {
-        let empty:UIColor = UIColor.colorHash(name: nil)
+        let empty: UIColor = UIColor.colorHash(name: nil)
         XCTAssert(empty == UIColor.red, "Short be red")
 
-        let first:UIColor = UIColor.colorHash(name: "Paul CLAUDOT")
+        let first: UIColor = UIColor.colorHash(name: "Paul CLAUDOT")
         var r: CGFloat = 0, g: CGFloat = 0, b: CGFloat = 0, a: CGFloat = 0
         first.getRed(&r, green: &g, blue: &b, alpha: &a)
 
@@ -52,20 +52,20 @@ class UIImageViewLettersDemoTests: XCTestCase {
         XCTAssert(g >= 0.1, "Short be >= 0.1")
         XCTAssert(b <= 0.84, "Short be <= 0.84")
         XCTAssert(b >= 0.1, "Short be >= 0.1")
-        
-        let second:UIColor = UIColor.colorHash(name: "Other Name")
+
+        let second: UIColor = UIColor.colorHash(name: "Other Name")
         second.getRed(&r, green: &g, blue: &b, alpha: &a)
-        
+
         XCTAssert(r <= 0.84, "Short be <= 0.84")
         XCTAssert(r >= 0.1, "Short be >= 0.1")
         XCTAssert(g <= 0.84, "Short be <= 0.84")
         XCTAssert(g >= 0.1, "Short be >= 0.1")
         XCTAssert(b <= 0.84, "Short be <= 0.84")
         XCTAssert(b >= 0.1, "Short be >= 0.1")
-        
-        let third:UIColor = UIColor.colorHash(name: "Other")
+
+        let third: UIColor = UIColor.colorHash(name: "Other")
         third.getRed(&r, green: &g, blue: &b, alpha: &a)
-        
+
         XCTAssert(r <= 0.84, "Short be <= 0.84")
         XCTAssert(r >= 0.1, "Short be >= 0.1")
         XCTAssert(g <= 0.84, "Short be <= 0.84")
